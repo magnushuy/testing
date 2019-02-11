2246d2c21db1c06cfb804e2986a0547e3b82ce9a
PiotrKusnierz <piotr.kusnierz@interia.eu>
2019-02-11 12:57:46 +0100

    adminRegistrerKunde Done

--- TestingBank/DAL/adminDatabase.php
+++ TestingBank/DAL/adminDatabase.php
@@ -62,7 +62,7 @@ class AdminDB
             if($this->db->affected_rows < 1)
             {
                 $this->db->rollback();
-                return "Feil1";
+                return "Feil";
             }
         }
         
@@ -78,7 +78,7 @@ class AdminDB
         else
         {
             $this->db->rollback();
-            return "Feil2";
+            return "Feil";
         }
     }
     
